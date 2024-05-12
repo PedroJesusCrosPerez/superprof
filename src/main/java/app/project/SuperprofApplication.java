239fd04bdcc17f812c375e61fadef4be2a7967de
Pedro Jesus Cros Perez <pcroper1909@g.educaand.es>
2024-05-12 13:39:00 +0200

    Initial commit

---
+++ src/main/java/app/project/SuperprofApplication.java
@@ -0,0 +1,13 @@
+package app.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperprofApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperprofApplication.class, args);
+    }
+
+}
