5673049fecf39dd9baac51ba53a04b882b6ab155
Pedro <pcroper1909@g.educaand.es>
2024-05-15 21:45:33 +0200

    Creating example on App of Array of Subject

--- src/main/java/app/project/App.java
+++ src/main/java/app/project/App.java
@@ -1,7 +1,9 @@
 package app.project;
 
 import app.project.entity.Product;
+import app.project.entity.Subject;
 import app.project.repository.ProductRepository;
+import app.project.repository.SubjectRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -16,12 +18,20 @@ public class App {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
 
 //        Insertar datos de ejemplo
-        var productRepository = context.getBean(ProductRepository.class);
+//        var productRepository = context.getBean(ProductRepository.class);
+//
+//        productRepository.saveAll(Arrays.asList(
+//                new Product(null, "apple", 45.99, 5),
+//                new Product(null, "banana", 12.50, 10),
+//                new Product(null, "orange", 30.75, 8)
+//        ));
 
-        productRepository.saveAll(Arrays.asList(
-                new Product(null, "apple", 45.99, 5),
-                new Product(null, "banana", 12.50, 10),
-                new Product(null, "orange", 30.75, 8)
+        var subjectRepository = context.getBean(SubjectRepository.class);
+
+        subjectRepository.saveAll(Arrays.asList(
+                new Subject(null, "Informática"),
+                new Subject(null, "Matemáticas"),
+                new Subject(null, "Lengua")
         ));
     }
 
